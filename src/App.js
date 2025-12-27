13aa094d64eb271992c8925031b19b4107301222
Yassine El Ansari <you@example.com>
2025-12-27 18:44:10 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="wrapper">
+            <h1 className="glitch" data-text="SOMETHING IS COMING">
+                SOMETHING IS COMING
+            </h1>
+
+            <div className="tag">
+                Crafted by <span>Yassine El Ansari</span>
+            </div>
+        </div>
+    );
+}
+
+export default App;
