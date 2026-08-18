52adc199ee76bcdb77a759941f98d171cc9a3518
elansari111 <elansari-yas@upf.ac.ma>
2026-08-18 13:31:43 +0100

    feat: add interactive chatbot, animated background effect, and global layout wrapper

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,11 +3,13 @@ import Footer from './Footer'
 import ScrollToTop from '../ui/ScrollToTop'
 import Chatbot from '../ui/Chatbot'
 import SmoothScroll from '../ui/SmoothScroll'
+import BackgroundEffect from '../ui/BackgroundEffect'
 
 export default function Layout({ children }) {
     return (
         <SmoothScroll>
-            <div className="min-h-screen flex flex-col bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+            <div className="min-h-screen flex flex-col bg-transparent text-black dark:text-white transition-colors duration-300 relative">
+                <BackgroundEffect />
                 <Navbar />
                 <main className="flex-grow">
                     {children}
